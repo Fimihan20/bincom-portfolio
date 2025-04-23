104c4c31a65c4435fff62c44cc690f49d5899e56
Fimihan20 <122857980+Fimihan20@users.noreply.github.com>
2025-04-23 16:19:00 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Back to Top button functionality
+const backToTopButton = document.getElementById("backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
